03fd42b344b6f11f011d496cacbbc2805cbac5d6
ayhan5 <hey956@gmail.com>
2021-03-07 14:25:56 -0500

    2.msg 07/03/21

--- src/gitpaketi/Class1.java
+++ src/gitpaketi/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloooooooo");
-
+        System.out.println("2.msg 07/03/21 ");
 	}
 
 }
