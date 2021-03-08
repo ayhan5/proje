169b77c4d55931bdec1ebbe2c11199f286a62c2c
ayhan5 <hey956@gmail.com>
2021-03-07 23:56:22 -0500

    updated page design

--- src/gitpaketi/Class1.java
+++ src/gitpaketi/Class1.java
@@ -5,10 +5,13 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloooooooo");
-        System.out.println("2.msg 07/03/21 ");
-        System.out.println("3.msg 07/03/21 ");		
-	System.out.println("1.commit from your remote master branch");
-	System.out.println("2.commit from your remote master branch");
+		System.out.println("2.msg 07/03/21 ");
+		System.out.println("3.msg 07/03/21 ");
+		System.out.println("1.commit from your remote master branch");
+		System.out.println("2.commit from your remote master branch");
+		//updated page design
+		
+		
 	}
 
 }
